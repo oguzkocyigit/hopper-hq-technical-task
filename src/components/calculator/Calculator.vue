b85d045814e36cdfac9bbe1352d86ebdd63c8a30
Oguz Kocyigit <kocyigitoguz90@gmail.com>
2022-03-07 00:45:39 +0300

    first commit

---
+++ src/components/calculator/Calculator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="result-wrapper">
+    <div class="columns">
+      <div class="column is-12">
+        <p>Hours Saved</p>
+        <p>388</p>
+      </div>
+      <div class="column is-12">
+        <p>Money Saved</p>
+        <p>$18,635</p>
+      </div>
+      <div class="column is-12">
+        <button>Try Waybook Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+}
+</script>
+
+<style lang="scss" src="./Result.scss"/>
+
---
+++ src/components/result/Result.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="result-wrapper">
+    <div class="column is-12">
+      <p>Hours Saved</p>
+      <p>388</p>
+    </div>
+    <div class="column is-12">
+      <p>Money Saved</p>
+      <p>$18,635</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+}
+</script>
+
+<style scoped>
+
+</style>
